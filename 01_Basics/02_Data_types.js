fe15367ea9749fface39d7479a0e1b170a2cfee2
GafarDesai <156561307+GafarDesai@users.noreply.github.com>
2024-04-17 12:14:00 +0530

    Add files via upload
    Commited 02_Data_types.js

---
+++ 01_Basics/02_Data_types.js
@@ -0,0 +1,41 @@
+//"use strict"
+"use strict" //That is using new code of javascript
+
+//Code readability
+console.log(3 
+    +
+    3); // Need of HIGH code readability
+
+console.log("Gafar")
+
+//Example
+let name = "Gafar Desai"
+let age = 24
+let state = null //if req temperature and database no record so response nulll value empty value.
+let islogged = true
+
+//Premitive data types of data types in javascript - Pre defined data types
+//1. Number -> 2 power 52/53 ex - num1 = 10, num2 = 10000
+//2. string -> Data in single or double quotes, mostly prefer double quotes
+//3. BigInt -> Mostly used for big numberic value, facebook, sharmarket
+//4. Null -> Empty value. It is data type also, object also
+//5. Boolean -> Either true or false
+//6. Undefined -> not defined the value but variable is there
+//7. Symbol -> Mostly used for uniquely identiy that data
+
+//Non-premitive data types - 
+//1. object
+
+//Looking for type of data using typeOf
+
+console.log(typeof "Gafar Desai");
+console.log(typeof null); //null type is object
+console.log(typeof undefined);
+console.log(typeof null);
+console.log(typeof(islogged));
+
+
+
+
+
+ 
